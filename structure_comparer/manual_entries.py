d54cfc38ce247e4fc420838b757ccfc379e27454
Alexander Essenwanger <a.essenwanger@gematik.de>
2024-03-06 07:11:28 +0000

    option to load manual mapping as yaml

--- structure_comparer/manual_entries.py
+++ structure_comparer/manual_entries.py
@@ -2,6 +2,8 @@ import copy
 import json
 from pathlib import Path
 
+import yaml
+
 from .classification import Classification
 
 MANUAL_ENTRIES_CLASSIFICATION = "classification"
@@ -19,7 +21,11 @@ class ManualEntries:
 
     def read(self, file: str | Path):
         self._file = Path(file)
-        data = json.loads(self._file.read_text(encoding="utf-8"))
+
+        if self._file.suffix == ".json":
+            data = json.loads(self._file.read_text(encoding="utf-8"))
+        elif self._file.suffix == ".yaml":
+            data = yaml.safe_load(self._file.read_text(encoding="utf-8"))
 
         # Interpret the classification as an enum
         for value in data.values():
@@ -36,7 +42,10 @@ class ManualEntries:
                 MANUAL_ENTRIES_CLASSIFICATION
             ].value
 
-        self._file.write_text(json.dumps(data, indent=4), encoding="utf-8")
+        if self._file.suffix == ".json":
+            self._file.write_text(json.dumps(data, indent=4), encoding="utf-8")
+        elif self._file.suffix == ".yaml":
+            self._file.write_text(yaml.safe_dump(data), encoding="utf-8")
 
     def __iter__(self):
         return iter(self._data["entries"])
