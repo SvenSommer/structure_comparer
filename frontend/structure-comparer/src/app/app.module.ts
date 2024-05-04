1a6a933792b20861a993e79d4666ddadb3047ba2
Robert Hoffmann <rob.hoffmann@posteo.de>
2024-05-04 18:42:19 +0000

    added first interactions to change classifications row based

--- frontend/structure-comparer/src/app/app.module.ts
+++ frontend/structure-comparer/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { MappingDetailComponent } from './mapping-detail/mapping-detail.componen
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
