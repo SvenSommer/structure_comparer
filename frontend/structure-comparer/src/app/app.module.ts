07e70bee456a62392c22a395c7c07071879315ca
Robert Hoffmann <rob.hoffmann@posteo.de>
2024-05-07 14:47:16 +0000

    added logic for searching, sorting and pagination

--- frontend/structure-comparer/src/app/app.module.ts
+++ frontend/structure-comparer/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,19 +14,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { MappingDetailComponent } from './mapping-detail/mapping-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MappingsListComponent,
-    MappingDetailComponent
-  ],
+  declarations: [AppComponent, MappingsListComponent, MappingDetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule 
+    FormsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/structure-comparer/src/app/mapping-detail/mapping-detail.component.ts
+++ frontend/structure-comparer/src/app/mapping-detail/mapping-detail.component.ts
@@ -3,23 +3,35 @@ import { ActivatedRoute } from '@angular/router';
 import { MappingsService } from '../mappings.service';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
-
-
+export interface IProfile {
+  name: string;
+  extra: string;
+  classification: string;
+  remark: string;
+  [key: string]: any;
+}
 @Component({
   selector: 'app-mapping-detail',
   templateUrl: './mapping-detail.component.html',
-  styleUrls: ['./mapping-detail.component.css']
+  styleUrls: ['./mapping-detail.component.css'],
 })
 export class MappingDetailComponent implements OnInit {
+  originalDetail: any;
   mappingDetail: any;
   availableFields: any[] = [];
-  editingIndex: number | null = null;
-  hoverIndex: number | null = null;
+  editingIndex: number | null | undefined = null;
+  hoverIndex: number | null | undefined = null;
+  filteredDetail: any;
+  // Paginator
+  totalLength: number = 0;
+  pageSize: number = 10;
+  pageIndex: number = 0;
+  pageSizeOptions: number[] = [10, 20, 50];
 
   constructor(
     private route: ActivatedRoute,
     private mappingsService: MappingsService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     const mappingId = this.route.snapshot.paramMap.get('id');
@@ -30,66 +42,168 @@ export class MappingDetailComponent implements OnInit {
   }
 
   loadMappingDetail(mappingId: string) {
-    this.mappingsService.getMappingDetail(mappingId)
-      .pipe(catchError(err => {
-        console.error('Error loading mapping detail', err);
-        return of({});
-      }))
-      .subscribe(mappingDetail => this.mappingDetail = mappingDetail);
+    this.mappingsService
+      .getMappingDetail(mappingId)
+      .pipe(
+        catchError((err) => {
+          console.error('Error loading mapping detail', err);
+          return of({});
+        })
+      )
+      .subscribe((mappingDetail) => {
+        this.totalLength = mappingDetail.fields.length;
+        this.originalDetail = mappingDetail;
+        this.mappingDetail = this.originalDetail;
+        this.filteredDetail = {
+          ...mappingDetail,
+          fields: mappingDetail.fields.slice(0, this.pageSize),
+        };
+      });
   }
 
   loadFields(mappingId: string) {
-    this.mappingsService.getMappingFields(mappingId)
-      .pipe(catchError(err => {
-        console.error('Error loading fields', err);
-        return of([]);
-      }))
-      .subscribe(fields => this.availableFields = fields.fields);
+    this.mappingsService
+      .getMappingFields(mappingId)
+      .pipe(
+        catchError((err) => {
+          console.error('Error loading fields', err);
+          return of([]);
+        })
+      )
+      .subscribe((fields) => (this.availableFields = fields.fields));
   }
 
   isProfilePresent(fieldProfiles: any[], profileName: string): boolean {
-    const profile = fieldProfiles.find(p => p.name === profileName);
+    const profile = fieldProfiles.find((p) => p.name === profileName);
     return !!profile?.present;
   }
 
-  startEditing(index: number): void {
-    this.editingIndex = index;
-  }
+  /**
+   * pagination, sort, filtering
+   * @param e : Sort | KeyboardEvent | PageEvent
+   * @returns new page
+   */
+  handleTable = (e: any) => {
+    const paginator = () => {
+      this.pageSize = e.pageSize;
+      this.pageIndex = e.pageIndex;
+      this.filteredDetail = {
+        ...this.mappingDetail,
+        fields: this.mappingDetail.fields.slice(
+          this.pageSize * this.pageIndex,
+          this.pageSize * (this.pageIndex + 1)
+        ),
+      };
+    };
 
-  cancelEditing() {
-    this.editingIndex = null;
-  }
+    const sorter = () => {
+      const data = this.filteredDetail.fields;
+      if (!e.active || e.direction === '') {
+        this.filteredDetail = { ...this.filteredDetail, fields: data };
+        return;
+      }
+      const sortedData = data.sort((a: IProfile, b: IProfile) => {
+        const isAsc = e.direction === 'asc';
+        const otherCondition = (t: any) =>
+          t['profiles'].find((profile: any) => profile.name === e.active)
+            .present;
 
-  startHover(index: number): void {
-    if (this.editingIndex === null) {
-      this.hoverIndex = index;
-    }
-  }
+        switch (e.active) {
+          case 'name':
+          case 'remark':
+            return compare(a[e.active], b[e.active], isAsc);
+          case 'extra':
+            return compare(
+              a['classification'] + a['extra'],
+              b['classification'] + b['extra'],
+              isAsc
+            );
+          default:
+            return compare(otherCondition(a), otherCondition(b), isAsc);
+        }
+      });
 
-  stopHover(): void {
-    this.hoverIndex = null;
-  }
+      this.filteredDetail = {
+        ...this.filteredDetail,
+        fields: sortedData,
+      };
+    };
+
+    const filter = () => {
+      const val = (e.target as HTMLInputElement).value.trim().toLowerCase();
+      const filterCond = (record: IProfile) => {
+        return (
+          !val.length ||
+          record.name.toLowerCase().indexOf(val) >= 0 ||
+          record.remark.toLowerCase().indexOf(val) >= 0 ||
+          record.classification.toLowerCase().indexOf(val) >= 0 ||
+          record.extra?.toLowerCase().indexOf(val) >= 0
+        );
+      };
+      this.mappingDetail = {
+        ...this.mappingDetail,
+        fields: this.originalDetail.fields.filter(filterCond),
+      };
+      this.totalLength = this.mappingDetail.fields.length;
+      this.pageIndex = 0;
+      this.filteredDetail = {
+        ...this.mappingDetail,
+        fields: this.mappingDetail.fields.slice(
+          this.pageSize * this.pageIndex,
+          this.pageSize * (this.pageIndex + 1)
+        ),
+      };
+    };
+
+    return { paginator, sorter, filter };
+  };
+
+  /**
+   * start, stop of hovering, editing
+   * @param idx : number
+   * @returns
+   */
+  handleEdit = (idx?: number) => {
+    const startHover = () => {
+      if (this.editingIndex === null) {
+        this.hoverIndex = idx;
+      }
+    };
+
+    const stopHover = () => {
+      this.hoverIndex = null;
+    };
+
+    const startEdit = () => {
+      this.editingIndex = idx;
+    };
+
+    const cancelEdit = () => {
+      this.editingIndex = null;
+    };
+
+    return { startHover, stopHover, startEdit, cancelEdit };
+  };
 
   getClassificationCssClass(classification: string): string {
-    const CSS_CLASS: {[key: string]: string} = {
-      'use': "row-use",
-      'not_use': "row-not-use",
-      'empty': "row-empty",
-      'extension': "row-extension",
-      'manuel': "row-manual",
-      'other': "row-other",
-      'copy_from': "row-copy-from",
-      'copy_to': "row-copy-to",
-      'fixed': "row-fixed",
-      'medication_service': "row-medication-service",
+    const CSS_CLASS: { [key: string]: string } = {
+      use: 'row-use',
+      not_use: 'row-not-use',
+      empty: 'row-empty',
+      extension: 'row-extension',
+      manual: 'row-manual',
+      other: 'row-other',
+      copy_from: 'row-copy-from',
+      copy_to: 'row-copy-to',
+      fixed: 'row-fixed',
+      medication_service: 'row-medication-service',
     };
     return CSS_CLASS[classification] || '';
   }
-  
 
   confirmChanges(field: any) {
     const updateData: any = {};
-  
+
     switch (field.userClassification) {
       case 'copy_from':
         //It would be nice to also define it this way!
@@ -98,7 +212,7 @@ export class MappingDetailComponent implements OnInit {
         updateData.target = field.targetField;
         break;
       case 'fixed':
-        // Seems like there is an ug in the backend, if you want to send two times a different fixed value 
+        // Seems like there is an ug in the backend, if you want to send two times a different fixed value
         updateData.fixed = field.fixedValue;
         break;
       case 'not_use':
@@ -115,18 +229,24 @@ export class MappingDetailComponent implements OnInit {
         console.error('Unknown userClassification:', field.userClassification);
         break;
     }
-  
-    console.log('Update Data:', updateData);  // Debug-Ausgabe der zu sendenden Daten
-  
+
+    console.log('Update Data:', updateData); // Debug-Ausgabe der zu sendenden Daten
+
     // Senden der Daten ans Backend
-    this.mappingsService.updateMappingField(this.mappingDetail.id, field.id, updateData).subscribe({
-      next: () => {
-        this.loadMappingDetail(this.mappingDetail.id);
-      },
-      error: err => console.error('Failed to update field', err)
-    });
-  
+    this.mappingsService
+      .updateMappingField(this.mappingDetail.id, field.id, updateData)
+      .subscribe({
+        next: () => {
+          this.loadMappingDetail(this.mappingDetail.id);
+        },
+        error: (err) => console.error('Failed to update field', err),
+      });
+
     // Beenden des Bearbeitungsmodus
-    this.cancelEditing();
+    this.handleEdit().cancelEdit();
   }
 }
+
+const compare = (a: number | string, b: number | string, isAsc: boolean) => {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+};
