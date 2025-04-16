c424653f3168a41f4473fcdf78b66196a4662da8
Alexander Essenwanger <a.essenwanger@gematik.de>
2025-04-16 08:26:52 +0200

    use Classification for mapping input validation

--- service/src/structure_comparer/model/mapping_input.py
+++ service/src/structure_comparer/model/mapping_input.py
@@ -1,25 +1,9 @@
 from pydantic import BaseModel
 
+from ..classification import Classification
+
 
 class MappingInput(BaseModel):
-    # properties:
-    #   action:
-    #     type: string
-    #     enum:
-    #       - copy_from
-    #       - copy_to
-    #       - fixed
-    #       - use
-    #       - not_use
-    #       - empty
-    #     description: Which action should be performed
-    #   target:
-    #     type: string
-    #     description: Field that is targetted (for copy actions)
-    #   value:
-    #     type: string
-    #     description: The fixed value
-    # pass
-    action: str
+    action: Classification
     target: str | None = None
     value: str | None = None
